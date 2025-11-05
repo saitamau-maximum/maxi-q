67dd748e755bfd8a0be9738e1a34412b2ac46ac8
nomanoma121 <107801494+nomanoma121@users.noreply.github.com>
2025-11-05 21:30:07 +0900

    chore: 必要そうなパッケージ追加 (#8)

--- packages/client/app/routes/home.tsx
+++ packages/client/app/routes/home.tsx
@@ -1,13 +1,18 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { css } from "styled-system/css";
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
+// export function meta({}: Route.MetaArgs) {
+//   return [
+//     { title: "New React Router App" },
+//     { name: "description", content: "Welcome to React Router!" },
+//   ];
+// }
 
 export default function Home() {
-  return <Welcome />;
+  return <div>
+    <p className={css({
+      fontSize: "20px",
+      fontWeight: "bold",
+      color: "blue.500",
+    })}>Welcome to the Home Page!</p>
+  </div>
 }
