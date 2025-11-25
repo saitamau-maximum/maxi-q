f2cb180a3d792df36943188bbf0aa736db246f3a
hinemosu1415 <167418833+hinemosu1415@users.noreply.github.com>
2025-11-25 11:24:46 +0900

    fix:ファイル名を修正2

--- packages/client/app/routes.ts
+++ packages/client/app/routes.ts
@@ -2,6 +2,6 @@ import { index, route, type RouteConfig } from "@react-router/dev/routes";
 
 export default [
     index("routes/home.tsx"),
-    route("/postQues", "routes/postQues.tsx"),
+    route("/postQues", "routes/postQuestion.tsx"),
 ] satisfies RouteConfig;
 
