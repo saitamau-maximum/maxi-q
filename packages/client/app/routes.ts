45ff3821c8d97bd3cdd7fb3417fb2696c02807ae
Danbooru06 <danboru1016@icloud.com>
2025-12-01 19:07:30 +0900

    feat:拡張子を付けた

--- packages/client/app/routes.ts
+++ packages/client/app/routes.ts
@@ -6,17 +6,17 @@ import {
 } from "@react-router/dev/routes";
 
 export default [
-	route("login", "routes/login"),
-	route("register", "routes/register"),
+	route("login", "routes/login.tsx"),
+	route("register", "routes/register.tsx"),
 	...prefix("users", [
-		route("me/posts", "routes/users/me/posts"),
-		route(":id/answers", "routes/users/:id/answers"),
+		route("me/posts", "routes/users/me/posts.tsx"),
+		route(":id/answers", "routes/users/:id/answers.tsx"),
 	]),
 	...prefix("posts", [
-		route(":id", "routes/posts/:id"),
-		route(":id/answer", "routes/posts/:id/answer"),
+		route(":id", "routes/posts/:id.tsx"),
+		route(":id/answer", "routes/posts/:id/answer.tsx"),
 	]),
-	route("post", "routes/post"),
-	route("timeline", "routes/timeline"),
-	index("routes/home"),
+	route("post", "routes/post.tsx"),
+	route("timeline", "routes/timeline.tsx"),
+	index("routes/home.tsx"),
 ] satisfies RouteConfig;
